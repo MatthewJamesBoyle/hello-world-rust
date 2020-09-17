b28eceacc48bbefdc0728384c3240c02d27317e9
Matthew Boyle <matthewboyle@Matthews-MacBook-Pro.local>
2020-09-17 08:18:47 +0100

    hello world rust

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,20 @@
 fn main() {
-    print_message("Hello World")
+    println!("{}", get_message("Hello World"))
 }
 
-fn print_message(msg: &str) {
-    println!("{}", msg)
+fn get_message(msg: &str) -> String {
+    format!("Your message was: {}. Welcome to rust.", msg)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_print_message() {
+        assert_eq!(
+            get_message("hello world"),
+            "Your message was: hello world. Welcome to rust.",
+        )
+    }
 }
